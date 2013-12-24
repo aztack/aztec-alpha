76f5deeb4e624d36746f80e0901fb63e02639565
aztack <aztack@163.com>
2013-12-24 23:48:15 +0800

    update dom.js

--- src/browser/dom.js
+++ src/browser/dom.js
@@ -6,15 +6,32 @@
 		_enum: $root.lang.enumerable
 	},
 	exports: [
+		script,
+		stylesheet,
+		internalScript,
+		internalStylesheet
 	]
 });
+
+function ReadyToAttach(node, defaultParent) {
+	return {
+		node: node,
+		append: function(target){
+			(target || defaultParent).appendChild(s);
+		},
+		prepend: function(target){
+			var where = target || defaultParent;
+			where.insertBefore(s, where.firstChild);
+		}
+	};
+}
 
 /**
  * create a script tag ready to attach to dom
  * @param  {String} src url of script
  * @param  {Function} cbk callback when script loaded or error occured
  * @param  {Object} opt {async,charset}
- * @return {Object}     {node,append,prepend}
+ * @return {Object} {node,append,prepend}
  */
 function script(src, cbk, opt){
 	var tag = document.getElementsByTagName('head')[0] || document.documentElement,
@@ -39,38 +56,50 @@ function script(src, cbk, opt){
 	if(opt.charset) s.charset = opt.charset;
 	s.onreadystatechange = s.onload = s.onerror = eventHandler;
 
-	return {
-		node: s,
-		append: function(target){
-			(target || tag).appendChild(s);
-		},
-		prepend: function(target){
-			var where = target || tag;
-			where.insertBefore(s, where.firstChild);
-		}
-	};
+	return ReadyToAttach(s, tag);
 }
 
 /**
  * create a stylesheet ready to attach to dom
  * @param  {String} href href of stylesheet
- * @return {Object}      {node,append,prepend}
+ * @return {Object} {node,append,prepend}
  */
 function stylesheet(href) {
 	var tag = document.getElementsByTagName('head')[0] || document.documentElement,
-		css = document.createElement('link');
-	css.rel = 'stylesheet';
-	css.type = 'text/css';
-	css.href = href;
+		link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.type = 'text/css';
+	link.href = href;
 
-	return {
-		node: css,
-		append: function(target){
-			(target || tag).appendChild(s);
-		},
-		prepend: function(target){
-			var where = target || tag;
-			where.insertBefore(s, where.firstChild);
-		}
-	};
+	return ReadyToAttach(link, tag);
+}
+
+/**
+ * create a internal stylesheet ready to attach to dom
+ * @param  {String} cssText
+ * @return {Object} {node,append,prepend}
+ */
+function internalStylesheet(cssText) {
+	var link = stylesheet(),
+		node = link.node;
+	if (node.styleSheet) {
+		node.styleSheet.cssText = cssText;
+	} else {
+		node.appendChild(document.createTextNode(cssText));
+	}
+	return link;
+}
+
+/**
+ * create a internal script tag ready to attach to dom
+ * @param  {String} js javascript code
+ * @return {Object} {node, append, prepend}
+ */
+function internalScript(js) {
+	var tag = document.body,
+		s = document.createElement('script');
+	s.type = 'text/javascript';
+	s.appendChild(document.createTextNode(js));
+
+	return ReadyToAttach(s, tag);
 }
